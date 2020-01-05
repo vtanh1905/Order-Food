1f4175c13a675c017a49e9332951c463563188c9
vtanh1905 <vtanh1905@gmail.com>
2020-01-05 18:12:07 +0700

    fix silder

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect, useHistory } from "react-router-dom";
 import "antd/dist/antd.css";
 import { Layout, Menu, Icon } from "antd";
 
@@ -13,6 +13,8 @@ const { Header, Content, Footer, Sider } = Layout;
 
 export default function App() {
   const [collapsed, setCollapsed] = useState(false);
+  const history = useHistory();
+  console.log(history.location.pathname);
 
   const onCollapse = val => {
     setCollapsed(val);
@@ -32,26 +34,26 @@ export default function App() {
             onCollapse={value => onCollapse(value)}
           >
             <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-              <Menu.Item key="1">
+            <Menu theme="dark" defaultSelectedKeys={[history.location.pathname]} mode="inline">
+              <Menu.Item key="/admin/user">
                 <Link to="/admin/user" className="d-flex align-items-center">
                   <Icon type="user" />
                   <span>Người dùng</span>
                 </Link>
               </Menu.Item>
-              <Menu.Item key="2">
+              <Menu.Item key="/admin/food">
                 <Link to="/admin/food" className="d-flex align-items-center">
                   <Icon type="desktop" />
                   <span>Thức ăn</span>
                 </Link>
               </Menu.Item>
-              <Menu.Item key="3">
+              <Menu.Item key="/admin/order">
                 <Link to="/admin/order" className="d-flex align-items-center">
                   <Icon type="file" />
                   <span>Đơn hàng</span>
                 </Link>
               </Menu.Item>
-              <Menu.Item key="4">
+              <Menu.Item key="/admin/statistic">
                 <Link to="/admin/statistic" className="d-flex align-items-center">
                   <Icon type="line-chart" />
                   <span>Thống kê</span>
