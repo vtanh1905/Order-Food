47fa00fffc70bed1b9cf4c343bc130177f4e44a0
tlvietdanh <tlvietdanh@gmail.com>
2020-01-06 06:39:00 +0700

    awdawd

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, Form } from 'react-bootstrap';
-import { Select, Avatar, Dropdown, Menu, Icon, Layout } from 'antd';
+import { Select, Avatar, Dropdown, Menu, Icon, Layout, Col, Row, Button, Input, Divider, notification } from 'antd';
 
 import './Layout.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const { Option } = Select;
-const { Content } = Layout;
+const { Content, Footer } = Layout;
 
 export default class LayoutHeader extends Component {
 
@@ -35,7 +35,7 @@ export default class LayoutHeader extends Component {
   render() {
     return (
       <Layout className="layout">
-        <Navbar className="shadow" bg="light" expand="lg" style={{ zIndex: 3 }}>
+        <Navbar className="shadow" bg="dark" expand="lg" style={{ zIndex: 3 }}>
           <Navbar.Brand href="/">
             <img
               src="https://www.upsieutoc.com/images/2020/01/05/logo.jpg"
@@ -65,6 +65,63 @@ export default class LayoutHeader extends Component {
         <Content className='fill-content'>
           <div style={{ background: '#fff' }} >{this.props.children}</div>
         </Content>
+        <Footer style={{ textAlign: 'center', padding: 'unset', zIndex: 999 }}>
+          <div style={{ background: '#585656', height: 430, color: 'white', padding: 80 }}>
+            <Row>
+              <Col span={6} style={{ textAlign: 'left' }}>
+                <p style={{ fontSize: 30, color: 'white' }}>LUCH APP</p>
+                <div style={{ marginBottom: 20 }}>
+                  <Icon style={{ fontSize: '25px', marginRight: 20 }} type="facebook" />
+                  <Icon style={{ fontSize: '25px', marginRight: 20 }} type="google" />
+                  <Icon style={{ fontSize: '25px', marginRight: 20 }} type="youtube" />
+                  <Icon style={{ fontSize: '25px', marginRight: 20 }} type="twitter" />
+                </div>
+                <p style={{ fontSize: 25, color: 'orange' }}>Liên lạc</p>
+                <div>
+                  <Icon type="phone" />
+                  <p style={{ fontSize: 20, color: 'white', marginLeft: 10, display: 'inline' }}>+84 962 074 802</p>
+                </div>
+                <div>
+                  <Icon type="mail" />
+                  <p style={{ fontSize: 20, color: 'white', marginLeft: 10, display: 'inline' }}>admin@lunchapp.com</p>
+                </div>
+              </Col>
+              <Col span={6} style={{ textAlign: 'left' }}>
+                <p style={{ fontSize: 25, color: 'orange' }}>Thông tin</p>
+                <Link style={{ fontSize: 20, color: 'white' }} to="/about"> <p>Về chúng tôi</p> </Link>
+                <Link style={{ fontSize: 20, color: 'white' }} to="/about"> <p>FAQ</p> </Link>
+                <Link style={{ fontSize: 20, color: 'white' }} to="/about"> <p>Thông tin thêm</p> </Link>
+                <Link style={{ fontSize: 20, color: 'white' }} to="/about"> <p>Công nghệ</p> </Link>
+              </Col>
+              <Col span={6} style={{ textAlign: 'left' }}>
+                <p style={{ fontSize: 25, color: 'orange' }}>Đường dây nóng</p>
+                <p style={{ fontSize: 20, color: 'white' }}>Dịch vụ</p>
+                <p style={{ fontSize: 20, color: 'white' }}>Hỗ trợ</p>
+                <p style={{ fontSize: 20, color: 'white' }}>Điều khoản và điều kiện</p>
+                <p style={{ fontSize: 20, color: 'white' }}>Các chính sách</p>
+              </Col>
+              <Col span={6} style={{ textAlign: 'left' }}>
+                <div style={{ marginBottom: 20 }}>
+                  <Icon style={{ fontSize: '25px', marginRight: 10 }} type="mail" />
+                  <p style={{ fontSize: 20, color: 'white', marginLeft: 10, display: 'inline' }}>Nhận các thông tin liên quan đến Luch App</p>
+                </div>
+                <Form>
+                  <Input placeholder="Nhập email của bạn" />
+          <Button style={{ background: 'red', color: 'white', marginTop: 20 }}onClick={() => {
+            notification['success']({
+              message: 'Information',
+              description:'Theo dõi thành công',
+            });
+          }}> Đăng ký</Button>
+                </Form>
+              </Col>
+            </Row>
+            <Divider></Divider>
+            <Row>
+              Lunch App ©2019. All Rights Reveived.
+            </Row>
+          </div>
+        </Footer>
       </Layout>
     )
   }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -17,25 +17,27 @@ function Order() {
   }
   return (
     <LayoutHeader style={{ width: '100%', height: '100%', overflow: 'hidden' }} >
-      <Carousel autoplay style={{ width: '100%', height: '100%', position: 'fixed', top: 55, left: 0, zIndex: 1 }} >
+      <Carousel autoplay style={{ width: '100%', height: '100%', position: 'absolute', top: 55, left: 0, zIndex: 0 }} >
         <div>
-          <img src="https://initiate.alphacoders.com/images/772/stretched-1920-1080-772596.jpg?1988" alt=""></img>
+          <img src="https://initiate.alphacoders.com/images/105/stretched-1920-1080-1058544.jpg?9437" alt=""></img>
           {}
         </div>
         <div>
-          <img src="https://initiate.alphacoders.com/images/492/stretched-1920-1080-49262.jpg?2327" alt=""></img>
+          <img src="https://initiate.alphacoders.com/images/762/stretched-1920-1080-76200.jpg?7637" alt=""></img>
         </div>
         <div>
-          <img src="https://initiate.alphacoders.com/images/284/stretched-1920-1080-284366.jpg?255" alt=""></img>
+          <img src="https://initiate.alphacoders.com/images/327/stretched-1920-1080-327640.jpg?2371" alt=""></img>
         </div>
         <div>
-          <img src="https://initiate.alphacoders.com/images/464/stretched-1920-1080-464030.jpg?5439" alt=""></img>
+          <img src="https://initiate.alphacoders.com/images/117/stretched-1920-1080-117895.jpg?8812" alt=""></img>
         </div>
         
       </Carousel>
-      <div style={{width: '100%', height: '100%', position: 'fixed', top: 55, left: '0%', zIndex: 2, background: 'black', opacity: 0.3 }}></div>
-      <div style={{width: '60%', height: '70%', position: 'fixed', top: '10%', left: '20%', zIndex: 3 }}>
-      <div className="container">
+      <div style={{width: '100%', height: '100%', position: 'absolute', top: 55, left: '0%', zIndex: 1, background: 'black', opacity: 0.3 }}></div>
+      {/* <div style={{width: '100%', height: '100%', position: 'fixed', top: 55, left: '0%', zIndex: 2, background: 'black', opacity: 0.3 }}></div> */}
+      {/* <div style={{width: '60%', height: '70%', position: 'fixed', top: '10%', left: '20%', zIndex: 3 }}> */}
+      <div className="homee">
+      <div className="container" style={{ zIndex: 2 }}>
           <Row><Avatar size={100} src="https://image.flaticon.com/icons/png/512/2362/2362817.png" shape="square" style={{ marginBottom: 20 }}></Avatar></Row>
           <Row><h1 style={{ color: 'black' }}>Chào mừng bạn đến với Lunch App</h1></Row>
           <Divider></Divider>
