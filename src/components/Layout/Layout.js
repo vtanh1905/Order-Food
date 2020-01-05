af1ce401fb5e11db630ecf65d681d0783d30ee37
cuongphung09 <43229377+cuongphung09@users.noreply.github.com>
2020-01-05 21:30:18 +0700

    9.30 5.1.2020

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -36,17 +36,17 @@ export default class LayoutHeader extends Component {
     return (
       <Layout className="layout">
         <Navbar className="shadow" bg="light" expand="lg" style={{ zIndex: 3 }}>
-          <Navbar.Brand href="/">
-            <img
+          <Navbar.Brand href="/">LUNCH APP
+            {/* <img
               src="https://www.upsieutoc.com/images/2020/01/05/logo.jpg"
               alt="logo.jpg"
               border="0"
-              // style={{ width: `50%`, height: `50%` }}
-              style={{position: `absolute`,
-                top: `10px`,
-                left: `10px`
-            }}
-            />{" "}
+              style={{ width: `50%`, height: `50%` }}
+              // style={{position: `absolute`,
+              //   top: `10px`,
+              //   left: `10px`
+            // }}
+            /> */}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
