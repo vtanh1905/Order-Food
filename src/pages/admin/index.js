fd814f840f6481bec15aa2067b51fbbdf3d633e0
phuocanPC <phuocantd@gmail.com>
2020-01-05 23:01:48 +0700

    edit location

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
 import "antd/dist/antd.css";
 import { Layout, Menu, Icon } from "antd";
 
-import './index.css';
+import "./index.css";
 import Order from "./order";
 import Food from "./food";
 import User from "./user";
@@ -11,17 +18,14 @@ import Statistic from "./statistic";
 
 const { Header, Content, Footer, Sider } = Layout;
 
-export default function App() {
+function App({ location }) {
   const [collapsed, setCollapsed] = useState(false);
 
   const onCollapse = val => {
     setCollapsed(val);
   };
 
-  const NoMatch = ({ location }) => (
-    <Redirect to="/admin/user" />
-  )
-
+  const NoMatch = ({ location }) => <Redirect to="/admin/user" />;
   return (
     <Router>
       <div>
@@ -32,33 +36,46 @@ export default function App() {
             onCollapse={value => onCollapse(value)}
           >
             <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-              <Menu.Item key="1">
+            <Menu
+              theme="dark"
+              defaultSelectedKeys={[`${location.pathname}`]}
+              mode="inline"
+            >
+              <Menu.Item key="/admin/user">
                 <Link to="/admin/user" className="d-flex align-items-center">
                   <Icon type="user" />
                   <span>Người dùng</span>
                 </Link>
               </Menu.Item>
-              <Menu.Item key="2">
+              <Menu.Item key="/admin/food">
                 <Link to="/admin/food" className="d-flex align-items-center">
                   <Icon type="desktop" />
                   <span>Thức ăn</span>
                 </Link>
               </Menu.Item>
-              <Menu.Item key="3">
+              <Menu.Item key="/admin/order">
                 <Link to="/admin/order" className="d-flex align-items-center">
                   <Icon type="file" />
                   <span>Đơn hàng</span>
                 </Link>
               </Menu.Item>
-              <Menu.Item key="4">
-                <Link to="/admin/statistic" className="d-flex align-items-center">
+              <Menu.Item key="/admin/statistic">
+                <Link
+                  to="/admin/statistic"
+                  className="d-flex align-items-center"
+                >
                   <Icon type="line-chart" />
                   <span>Thống kê</span>
                 </Link>
               </Menu.Item>
               <Menu.Item key="5">
-                <div className="d-flex align-items-center" onClick={() => { window.location.replace('/'); localStorage.clear() }}>
+                <div
+                  className="d-flex align-items-center"
+                  onClick={() => {
+                    window.location.replace("/");
+                    localStorage.clear();
+                  }}
+                >
                   <Icon type="logout" />
                   <span>Đăng xuất</span>
                 </div>
@@ -93,3 +110,5 @@ export default function App() {
     </Router>
   );
 }
+
+export default withRouter(App);
